e3863d4016e93fb87773425faf9569b86fc78024
CoreJava58 <En_Enter@aliyun.com>
2016-03-19 03:05:05 +0800

    Create User Class

---
+++ Core/com.core.model/User.java
@@ -0,0 +1,39 @@
+
+public class User {
+
+	private Integer id;
+	private String userName;
+	private String userAddress;
+	private int age;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserAddress() {
+		return userAddress;
+	}
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + userName + ", userAddress=" + userAddress + ", age=" + age + "]";
+	}
+	
+	
+	
+}
